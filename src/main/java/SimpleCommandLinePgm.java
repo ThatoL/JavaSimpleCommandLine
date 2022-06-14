111fbbe40ff9643c5aad5f7634e78bb15c90bd75
ThatoL <t.lebethe93@gmail.com>
2022-06-14 12:03:13 +0200

    first commit

---
+++ src/main/java/SimpleCommandLinePgm.java
@@ -0,0 +1,12 @@
+
+
+
+public class SimpleCommandLinePgm {
+    public static void main(String[] args) {
+        System.out.println("CLA Program");
+        System.out.println("The CLA for the program are: ");
+        for (String str: args) {
+            System.out.println(args);
+        }
+    }
+}
